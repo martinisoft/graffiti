2d8d81376bbe8ab11e1870776915bb09b3e7eaad
Aaron Kalin <akalin@martinisoftware.com>
2011-05-18 08:41:48 -0400

    Change load path for environment

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,5 +1,4 @@
-$LOAD_PATH.unshift(File.dirname(__FILE__) + '/../../lib')
-require 'graffiti'
+require File.expand_path('../../../lib/graffiti', __FILE__)
 require 'aruba/cucumber'
 require 'capybara/cucumber'
 Capybara.app = Graffiti::Server
